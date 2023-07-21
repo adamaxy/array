695cdc87f6b3cb285a8b0dc03067b0ce4e740ead
adamaxy <adam996@libero.it>
2023-07-21 13:50:55 +0200

    upload array ex

---
+++ test.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class test {
+    public static void main(String [] args ){
+    String [] carBrands = new String[5];
+        carBrands[0] = "mazda";
+        carBrands[1] = "bmw";
+        carBrands[2] = "audi";
+        carBrands[3] = "mercedes";
+        carBrands[4] = "ferrari";
+        System.out.println(carBrands[2]);
+
+int [] primeNumbers = new int[6];
+        primeNumbers[0] = 2;
+        primeNumbers[1] = 3 ;
+        primeNumbers[2] = 5;
+        primeNumbers[3] = 7;
+        primeNumbers[4] = 11;
+        primeNumbers[5] = 13;
+        System.out.print("Array contents: ");
+        for (int i = 0; i < primeNumbers.length; i++) {
+            System.out.print(primeNumbers[i] + " " );}
+
+
+
+}}
